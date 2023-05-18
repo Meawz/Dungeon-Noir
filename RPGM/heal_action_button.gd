5a5ac9a1cfd98f6628c22af0ebc2d910ae7d6498
Meawz <123901325+Meawz@users.noreply.github.com>
2023-05-18 20:48:11 +0200

    Delete RPGM directory

--- RPGM/heal_action_button.gd
+++
@@ -1,9 +0,0 @@
-extends "res://ActionButton.gd"
-
-func _on_pressed():
-	var playerStats = BattleUnits.PlayerStats
-	if playerStats != null:
-		if playerStats.mp >= 6:
-			playerStats.hp += 5
-			playerStats.mp -= 6
-			playerStats.ap -= 1
